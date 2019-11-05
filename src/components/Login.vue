4e9febd60f6cccb786a86fa35244b2e8b58f25ee
Alex Chiu <alexchiu11@gmail.com>
2019-11-05 20:27:25 +0000

    toggle between login and register mode

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,39 +1,55 @@
 <template>
-<div class="login-page">
-  <div class="form">
+  <div class="login-page">
+    <div class="form">
+      <form v-if="mode === 'register'" class="register-form stack">
+        <h2>👋 Register Here</h2>
+        <input type="text" placeholder="name" />
+        <input type="password" placeholder="password" />
+        <input type="text" placeholder="email address" />
+        <button>create</button>
+        <p class="message">
+          Already registered?
+          <a @click="toggleMode" href="#">Sign In</a>
+        </p>
+      </form>
 
-    <form v-if="mode === 'register'" class="register-form">
-      <h2>👋 Register Here</h2>
-      <input type="text" placeholder="name"/>
-      <input type="password" placeholder="password"/>
-      <input type="text" placeholder="email address"/>
-      <button>create</button>
-      <p class="message">Already registered? <a href="#">Sign In</a></p>
-    </form>
-    
-    <form v-else class="login-form">
-      <h2>🔐 Login Here</h2>
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
-      <button>login</button>
-      <p class="message">Not registered? <a href="#">Create an account</a></p>
-    </form>
-  
+      <form v-else class="login-form stack">
+        <h2>🔐 Login Here</h2>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>login</button>
+        <p class="message">
+          Not registered?
+          <a @click="toggleMode" href="#">Create an account</a>
+        </p>
+      </form>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        mode: "login"
+export default {
+  data() {
+    return {
+      mode: "register"
+    };
+  },
+  methods: {
+    toggleMode() { 
+      if (this.mode === "register") {
+        this.mode = "login";
+      } else {
+        this.mode = "register";
       }
-    },
-    
+    }
   }
+};
 </script>
 
 <style scoped>
-
+.stack {
+  display: flex;
+  flex-direction: column;
+  width: 20%;
+}
 </style>
