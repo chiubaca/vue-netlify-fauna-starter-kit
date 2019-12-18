81c10549918572b6403b57aac1f0529c54a822f2
Alex Chiu <alexchiu11@gmail.com>
2019-12-18 16:00:08 +0000

    moved dev panel out of profile compoent

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,27 +2,15 @@
   <div>
     <h1>Your Profile</h1>
     <p>🔒 You should be logged in to see this page 🔒</p>
-
-    
-    <button @click="updateUserMetaData">
-      Update user meta data
-    </button>
-
   </div>
 </template>
 
 <script>
-  import {mapActions} from "vuex"
-  export default {
 
-    methods: {
-      ...mapActions("auth",[
-        "updateUserMetaData"
-      ])
-    },
-  }
+export default {
+
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
