07a243d6df58cb01197d5a0d2bda630f3737e096
Alex Chiu <alexchiu11@gmail.com>
2019-12-28 12:02:14 +0000

    refactor: reused code frrom identity-signup

--- functions/identity-external-signup.js
+++ functions/identity-external-signup.js
@@ -8,45 +8,10 @@ an Authorisation header that has a valid JWT
 
 "use strict";
 const fetch = require("node-fetch");
-const faunadb = require("faunadb");
-const generator = require('generate-password');
-
-/* configure faunaDB Client with our secret 
-   DB Secret key is held within the netlify online UI
-*/
-const q = faunadb.query
-const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SERVER_SECRET
-})
-
-/* create a user in FaunaDB that can connect from the browser */
-/**
- * 
- * @param {object} userData
- * @property {string} userData.id - netlify id nunmber
- * @property {object} userData.user_metadata - additonal arbitary 
- * @param {string} password 
- */ 
-function createDbUser(userData, password) {
-  return client.query(q.Create(q.Collection("users"), {
-    credentials : {
-      password : password
-    },
-    data : {
-      id : userData.id,
-      user_metadata : userData.user_metadata
-    }
-  }))
-}
-
-function obtainToken(user, password) {
-  return client.query(
-    q.Login(q.Select("ref", user), { password }))
-}
+const identitySignup = require('./identity-signup')
 
 /**
  * Update the app_metadata for a netlify user to include add the faunaDB token
- * 
  * @param {object} appMetaDataObject - object containing any additional arbitary data for the user
  * @param {string} usersAdminUrl -  url of  eg "<SITE.com>/.netlify/identity/admin/users/123-abc-456"
  * @param {string} adminAuthHeader - authorisation JWT
@@ -54,9 +19,9 @@ function obtainToken(user, password) {
 function updateNetlifyUserAppMetaData (appMetaData, usersAdminUrl, JWT){
   
   return fetch(usersAdminUrl, {
-    method: "PUT",
-    headers: { Authorization: `Bearer ${JWT}` },
-    body: JSON.stringify({app_metadata: appMetaData})
+      method: "PUT",
+      headers: { Authorization: `Bearer ${JWT}` },
+      body: JSON.stringify({app_metadata: appMetaData})
     })
     .then(response => response.json())
     .then(data =>  data )
@@ -83,16 +48,12 @@ function handler(event, context, callback) {
     id : userID,
     user_metadata: user.user_metadata
   }
-  const password = generator.generate({
-  length: 10,
-  numbers: true
-  });
-
+  const password = identitySignup.generatePassword()
   console.log("admin url check", usersAdminUrl)
   console.log("bearer token check", JWT)
 
-  createDbUser(userObject, password)
-    .then((user) => obtainToken(user, password))
+  identitySignup.createDbUser(userObject, password)
+    .then((user) => identitySignup.obtainToken(user, password))
     .then((key) => updateNetlifyUserAppMetaData({db_token : key.secret} , usersAdminUrl, JWT))
     .then((resp) => {
       console.log("Received response: ", !!resp)
@@ -106,9 +67,7 @@ function handler(event, context, callback) {
       console.error("Unable to create a user account", error)
       callback(null, {
         statusCode: 418,
-        body: JSON.stringify({
-          error: error
-        })
+        body: JSON.stringify({error: error})
       })
     })
 }
--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -8,9 +8,15 @@ const client = new faunadb.Client({
   secret: process.env.FAUNADB_SERVER_SECRET
 })
 
-/* create a user in FaunaDB that can connect from the browser */
-function createUser(userData, password) {
-  return client.query(q.Create(q.Class("users"), {
+/**
+ * create a user in FaunaDB that can connect from the browser
+ * @param {object} userData
+ * @property {string} userData.id - netlify id nunmber
+ * @property {object} userData.user_metadata - additonal arbitary 
+ * @param {string} password 
+ */ 
+function createDbUser(userData, password) {
+  return client.query(q.Create(q.Collection("users"), {
     credentials : {
       password : password
     },
@@ -22,40 +28,47 @@ function createUser(userData, password) {
 }
 
 function obtainToken(user, password) {
-  console.log("creating FaunaDB token")
+  console.log("Generating new DB token")
   return client.query(
     q.Login(q.Select("ref", user), { password }))
 }
 
-function handler(event, context, callback) {
-  var payload = JSON.parse(event.body);
-  var userData = payload.user;
-
-  const password = generator.generate({
+function generatePassword(){
+  return generator.generate({
       length: 10,
       numbers: true
   });
+}
 
-  createUser(userData, password)
+function handler(event, context, callback) {
+
+  let payload = JSON.parse(event.body);
+  let userData = payload.user;
+  const password = generatePassword();
+
+  createDbUser(userData, password)
     .then((user) => obtainToken(user, password))
-    .then((key) => callback(null, {
-      // the if return status is 200 or 204 the function will get blocked
-      statusCode: 200, 
-      body: JSON.stringify({
-        app_metadata: {
-          //the return body will update the netlify user  
-          db_token : key.secret
-          // we discard the credential, and can create a new one if we ever need a new token
-          // faunadb_credential : password
-        } })
-    })).catch((e) => {
-      console.error(e)
+    .then((key) => {
+      console.log("Successfully created DB account")
+      callback(null, {
+        //If return status is 200 or 204 the function will get blocked
+        statusCode: 200,
+        //the return body will update the netlify user   
+        body: JSON.stringify({ app_metadata: { db_token : key.secret} })
+      })
+    })
+    .catch((e) => {
+      console.error("Somethings gone wrong ",e)
       callback(null, {
         statusCode: 500,
-        body: JSON.stringify({
-          error: e
-        })
+        body: JSON.stringify({error: e})
       })
     })
 }
-module.exports = {handler: handler};
+
+module.exports = {
+  handler: handler,
+  createDbUser: createDbUser,
+  obtainToken: obtainToken,
+  generatePassword: generatePassword
+};
