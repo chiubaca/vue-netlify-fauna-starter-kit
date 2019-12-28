7a68a6a6d07cd1bee7aa71ae54d7ac361feb15a7
Alex Chiu <alexchiu11@gmail.com>
2019-12-28 22:47:03 +0000

    split logic into two functions

--- src/helpers/authorise-tokens.js
+++ src/helpers/authorise-tokens.js
@@ -1,58 +1,57 @@
 /*
-helper file to validate tokens in the URL if they are present 
+Extract and validate tokens in the URL if they are present.
 */ 
 
 import store from '../store'
 
-export default function () {
-  
-  // hash from URL
-  const hash = (document.location.hash || "").replace(/^#\/?/, "");
-  
+function checkConfirmationToken(){
+
   let token = decodeURIComponent(window.location.search)
     .substring(1)
     .split("confirmation_token=")[1];
-
-  //regex to check for external access token url hash
  
-  // let jwt = window.location.hash.match(/access_token=/)
-  
-  // only authorise if a token is available
+  // Authorise if a token is available, otherwise exit early
   if (token) {
     console.log("got a token: ", token)
     store.dispatch("auth/attemptConfirmation", token)
       .then((resp) => {
-        alert(`Your account has been confirmed, please login`);
-        console.log(resp)
+        alert(`${resp.email} has been confirmed, please login`);
       })
       .catch(error => {
-        alert(`Can't Authorise your account right now, try again`)
+        alert(`Can't authorise your account right now. Please try again`)
         console.error(error, "Somethings gone wrong logging in")
       });
     return
   }
-  
+}
+
+function checkAccessToken(){
   const externalToken = /access_token=/;
+  
+  // Clean the URL
+  const hash = (document.location.hash || "").replace(/^#\/?/, "");
+  
   if (hash.match(externalToken)){
     console.log("detected external token", hash.match(externalToken) )
     //create token param from url hash
     const params = {};
+
     hash.split("&").forEach(pair => {
       const [key, value] = pair.split("=");
       params[key] = value;
     });
-    //could store this param as login cookie here...
 
     store.dispatch("auth/completeExternalLogin", params)
-    .then(() => {
-        alert("You have successfully signed in via external provider");
-      })
+      .then(() => {alert("You have successfully signed in via external provider")})
       .catch(error => {
         alert(`Can't Authorise your account right now, try again`)
         console.error(error, "Somethings gone wrong logging in")
       });
   }
+}
 
-  console.log("not a new user")
-
+export default function () {
+  
+  checkConfirmationToken();
+  checkAccessToken();
 }
