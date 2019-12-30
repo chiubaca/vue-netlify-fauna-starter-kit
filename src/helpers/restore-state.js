e3a9b3fa7b432dfa8b51f5c0fc52405db833c789
Alex Chiu <alexchiu11@gmail.com>
2019-12-30 16:21:24 +0000

    tidy up and added in comments

--- src/helpers/restore-state.js
+++ src/helpers/restore-state.js
@@ -1,14 +1,18 @@
+/**
+ * Restore user from local storage into vuex user store
+ */
+
 import store from '../store'
 
 export default function () {
-
   let savedUser = JSON.parse(localStorage.getItem("store")) ;
-
+  // No user in Local storage so exit early
   if (!savedUser){
-    console.log("No user found in local storage")
     return
   }
+  console.log(`Remembered user account:
+               - ${savedUser.currentUser.id}  
+               - ${savedUser.currentUser.email}`)
 
-  console.log("restoring the following user to app state", savedUser.currentUser)
   store.commit("auth/SET_CURRENT_USER", savedUser.currentUser)
 }
