75f4e98e3baa3bb29787e1daf32cdd1628fc1fec
Alex Chiu <alexchiu11@gmail.com>
2020-01-12 21:01:29 +0000

    removed restore state helper, vuexPersist does handles all the logic out the bo

--- src/store/index.js
+++ src/store/index.js
@@ -9,11 +9,9 @@ Vue.use(Vuex)
 const vuexLocalStorage = new VuexPersist({
   key: 'store', // The key to store the state on in the storage provider.
   storage: window.localStorage, // or window.sessionStorage or localForage
+  module:['auth', 'app']
   // Function that passes the state and returns the state with only the objects you want to store.
-  reducer: state => ({
-    currentUser:state.auth.currentUser,
-    siteURL:state.app.siteURL
-    }),
+  // reducer: state => ({}),
   // Function that passes a mutation and lets you decide if it should update the state in localStorage.
   // filter: mutation => (true)
 })
