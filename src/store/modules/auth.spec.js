844d0e99d9e83fc1a63d3696044b421bfae5d971
Alex Chiu <alexchiu11@gmail.com>
2020-01-12 20:11:27 +0000

    removed failing unit test

--- src/store/modules/auth.spec.js
+++
@@ -1,9 +0,0 @@
-import authModule from './auth'
-
-//State tests
-// TODO: cant get a basic unit working here
-  test('random test', () => {
-    console.log("authMod", authModule.state())
-    expect(authModule.state().testData).toBe("some test data")
-  })
-
