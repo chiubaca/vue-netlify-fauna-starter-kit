fcdf00f31df371b849ebdf135e6cd6baceba6fbb
Alex Chiu <alexchiu11@gmail.com>
2020-01-15 19:10:56 +0000

    stubbed tests todo

--- src/store/modules/auth.spec.js
+++ src/store/modules/auth.spec.js
@@ -1,10 +1,43 @@
 import authModule from './auth'
 
 //State tests
-describe('app/state', () => {
+describe('auth/state', () => {
   test('test', () => {
     expect(authModule.state().testData).toBe("some test data")
   })
 
+  //TODO
+  // - loggedIn
+  // - currentUser
+  // -  GoTrueAuth
 
+})
+
+//Mutations tests
+describe('auth/mutations', () => {
+  test('test', () => {
+  })
+
+  //TODO
+  // - SET_GOTRUE
+  // - SET_CURRENT_USER
+
+})
+
+//Actions tests
+describe('auth/mutations', () => {
+  test('test', () => {
+  })
+
+  //TODO
+  // - invokeSignupFunction
+  // - attemptLogin
+  // - attemptExternalLogin
+  // - completeExternalLogin
+  // - attemptSignup
+  // - attemptConfirmation
+  // - attemptLogout
+  // - getUserJWTToken
+  // - getCurrentUser
+  // - initAuth
 })
