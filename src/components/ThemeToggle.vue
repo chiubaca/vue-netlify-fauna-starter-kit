8faf9fd8aa80218b597688cb8963699f37b7a948
Alex Chiu <alexchiu11@gmail.com>
2020-01-21 20:23:25 +0000

    create a reusable theme toggle

---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input v-model="darkMode" type="checkbox" class="theme-switch" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      darkMode: false
+    };
+  },
+  watch: {
+    darkMode: function() {
+      // add/remove class to/from html tag
+      let htmlElement = document.documentElement;
+      if (this.darkMode) {
+        localStorage.setItem("theme", "dark");
+        htmlElement.setAttribute("theme", "dark");
+      } else {
+        localStorage.setItem("theme", "light");
+        htmlElement.setAttribute("theme", "light");
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
