9da163f9b7430cf4ff6543d980cdf10a6521f9db
Alex Chiu <alexchiu11@gmail.com>
2020-02-05 00:00:57 +0000

    added disclaimer in recover email component

--- src/components/RecoverAccount.vue
+++ src/components/RecoverAccount.vue
@@ -5,8 +5,16 @@
       class="stack"
       @keyup.enter="requestPasswordRecover(crendentials.email)"
     >
-      <h2>What account are you trying to recover?</h2>
-      <label for="email">Email</label>
+      <h2>Forgot Password?</h2>
+      <p>
+        Enter the email address you used when you joined and we’ll send you
+        instructions to reset your password.
+      </p>
+      <p>
+        For security reasons, we do NOT store your password. So rest assured
+        that we will never send your password via email.
+      </p>
+      <label for="email">Email Address</label>
       <input
         id="email"
         v-model="crendentials.email"
