6f64190d5d680d67befaf55432e89ce3bfd69f51
Alex Chiu <alexchiu11@gmail.com>
2020-04-01 18:14:27 +0100

    theme switcher styling overhaul

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <span v-if="darkMode === false"> 🌚 Dark Mode </span>
-    <span v-else> 🌞 Light Mode </span>
-    <input v-model="darkMode" type="checkbox" class="theme-switch" />
+    <label class="switch">
+      <input v-model="darkMode" type="checkbox" class="theme-switch" />
+      <span class="slider round"> <span>🌚</span> <span>🌞</span> </span>
+    </label>
   </div>
 </template>
 
@@ -29,4 +30,73 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  span {
+    padding: 5px;
+  }
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: #919191;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+.slider {
+  background-color: #b9b9b9;
+}
+input:checked + .slider {
+  background-color: black;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196f3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
