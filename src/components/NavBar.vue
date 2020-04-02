46168c4c4c1909ef8015df88523ad1aabce1a936
Alex Chiu <alexchiu11@gmail.com>
2020-04-02 22:58:00 +0100

    implemted simplier navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav id="nav-bar">
+    <div id="nav-items">
+      <router-link :to="{ name: 'home' }">🏠 Home</router-link>
+
+      <router-link :to="{ name: 'journals' }">📔 Your Journals</router-link>
+
+      <router-link :to="{ name: 'profile' }">🆔 Your Profile</router-link>
+
+      <ThemeToggle />
+    </div>
+  </nav>
+</template>
+
+<script>
+import ThemeToggle from "./ThemeToggle.vue";
+
+export default {
+  components: {
+    ThemeToggle
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+nav#nav-bar #nav-items {
+  display: flex;
+  flex-flow: wrap;
+  text-decoration: none;
+  align-items: center;
+  text-align: center;
+  justify-content: space-evenly;
+  background-color: var(--app-background-color);
+
+  a {
+    text-decoration: none;
+    font-weight: bold;
+  }
+
+  a:hover {
+    border-bottom: solid 3px var(--primary);
+  }
+}
+nav#nav-bar #nav-items > a,
+div {
+  padding: 30px 10px 0 10px;
+}
+</style>
