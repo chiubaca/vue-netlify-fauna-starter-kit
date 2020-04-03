d8d664deb0c51653b19bf3637ca9a03adaaa825c
Alex Chiu <alexchiu11@gmail.com>
2020-04-03 17:50:07 +0100

    greet username on home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -25,7 +25,7 @@
     </div>
     <Login v-if="currentUser === null" />
     <div v-else id="greeting">
-      <h2>🖐️ Welcome Back!</h2>
+      <h2>🖐️ Welcome Back {{ currentUser.user_metadata.full_name }}!</h2>
       <p>
         You're logged in. Go check out your
         <router-link :to="{ name: 'journals' }">journals ➡️ </router-link>.
@@ -68,6 +68,9 @@ export default {
   border-radius: 15px;
   border-color: #c6c6c6;
   border-width: 1px;
+  h2 {
+    text-transform: capitalize;
+  }
 }
 img#logo {
   width: 100%;
